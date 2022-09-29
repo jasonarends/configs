e101666334d20e63e936496c5f390f2bb9983295
Jason Arends <jason@lvm-debian-1.orionis>
2022-09-29 10:46:35 -0500

    zshrc

--- link.sh
+++ link.sh
@@ -3,9 +3,12 @@
 mv ~/.tcshrc ~/.tcshrc-old
 mv ~/.vim ~/.vim-old
 mv ~/.p10k.zsh ~/.p10k.zsh-old
-mv ~/.config/neofetch/config.conf ~/.config/neofetch/config.conf-old
+mv ~/.config/neofetch ~/.config/neofetch-old
+mv ~/.zshrc ~/.zshrc-old
+mv ~/.vimrc ~/.vimrc-old
 
 ln -s ~/configs/.vim ~/
 ln -s ~/configs/.tcshrc ~/
-ln -s ~/configs/.p10k.zsh
+ln -s ~/configs/.p10k.zsh ~/
 ln -s ~/configs/neofetch ~/.config/
+ln -s ~/configs/.zshrc ~/
