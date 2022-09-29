623c53bad6e2177386c1ee35df07a41984df0bca
Jason Arends <jason_arends@intuit.com>
2022-09-29 10:44:41 -0500

    edits

--- link.sh
+++ link.sh
@@ -3,9 +3,7 @@
 mv ~/.tcshrc ~/.tcshrc-old
 mv ~/.vim ~/.vim-old
 mv ~/.p10k.zsh ~/.p10k.zsh-old
-mv ~/.config/neofetch/config.conf ~/.config/neofetch/config.conf-old
 
 ln -s ~/configs/.vim ~/
 ln -s ~/configs/.tcshrc ~/
 ln -s ~/configs/.p10k.zsh
-ln -s ~/configs/neofetch ~/.config/
